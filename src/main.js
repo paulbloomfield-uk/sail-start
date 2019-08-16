94016bd7d76c584e0b24ec6ad7a0ed050cc8f2c8
Paul Bloomfield <paulbloomfield.uk@gmail.com>
2019-08-16 19:09:27 +0100

    Base with Font Awesome

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@fortawesome/fontawesome-free/css/all.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app');
